74228d520a866fa3e8cfe5767f7d27058c178098
Lndqw12 <COMP222012016@mail.ru>
2016-09-08 17:04:12 +0300

    PHPlabs

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<meta charset ="UTF-8">
+<?php
+$aq = "Получится - ";
+$a = 150;
+$b = 100;
+$sum = $a+$b;
+echo $aq, $sum;
+
+echo "</br>";
+
+$sergey = 33;
+if(isset($sergey));
+echo "переменная существует";
+
+echo "</br>";
+
+$varby = "test";
+if (isset($varby))
+{
+	echo "good";
+	}
+else
+{
+	echo "bad";
+	}
+?>
